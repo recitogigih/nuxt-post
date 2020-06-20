d60faa78ac2cfab3b9448c005c3a5857076c5743
Recito Gigih P <recitogigih9@gmail.com>
2020-06-20 21:54:12 +0700

    first commit

---
+++ components/PostItem.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="post">
+    <div class="post-content">
+      <a class="post-header post-header-link clickable">
+        <h4 class="title is-4">{{title}}</h4>
+        <h5 class="subtitle is-5">{{subtitle}} </h5>
+      </a>
+      <div class="post-footer">
+        by filip, {{date | formatDate}}
+      </div>
+    </div>
+    <div class="post-right">
+      <label class="checkbox">
+        <input type="checkbox" :checked="isRead">
+        Read
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+  import moment from 'moment'
+  export default {
+    data: () => ({
+      moment: moment
+    }),
+    props: {
+      title: {
+        type: String,
+        required: true,
+      },
+      subtitle: {
+        type: String,
+        required: false,
+      },
+      date: {
+        type: Date,
+        required: false,
+        default: new Date()
+      },
+      isRead:{
+        type:Boolean,
+        required:false
+      }
+    }
+  }
+
+</script>
+
+<style scoped lang="scss">
+
+
+  .post {
+    margin-bottom: 20px;
+    padding: 5px;
+    border-bottom: 2px solid transparent;
+    display: flex;
+    flex-direction: row;
+
+    &-footer {
+       font-style: italic;
+    }
+
+    &-content {
+      flex: 1;
+    }
+
+    &-right {
+      float: right;
+      justify-content: flex-end;
+      align-self: center;
+    }
+
+    &:hover{
+      border-bottom: 2px solid #e8e8e8;
+    }
+  }
+
+ 
+
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <navbar />
+
+    <div class="blogs-page">
+      <div class="main-content">
+        <div class="container">
+          <div class="columns is-mobile">
+            <div class="column is-8 is-offset-2">
+              <div class="section">
+                <div class="title">
+                  <h1>Newest Posts</h1>
+                  <hr>
+                </div>
+
+                <post-item v-for="post in posts" 
+                  :key="post._id" 
+                  :title="post.title" 
+                  :subtitle="post.subtitle" 
+                  :date="post.createdAt" 
+                  :isRead="post.isRead"
+                />
+
+              </div>
+              <!-- end of post -->
+            </div>
+            <!-- end of side bar -->
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Navbar from '~/components/Navbar'
+  import PostItem from '~/components/PostItem'
+  export default {
+    components: {
+      Navbar,
+      PostItem
+    },
+    data() {
+      return {
+        posts: [{
+            _id: 1,
+            title: 'My 1 post',
+            subtitle: 'My 1 subtitle',
+            createdAt: new Date(),
+            isRead: false
+          },
+          {
+            _id: 2,
+            title: 'My 2 post',
+            subtitle: 'My 2m  subtitle',
+            createdAt: new Date(),
+            isRead : true
+          },
+        ]
+      }
+    }
+  }
+
+</script>
